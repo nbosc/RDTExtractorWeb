9f085ae5321cbfdd1fb0b419a462d8fcc07bae37
IgnacioPasamontes <ignacio.pasamontes@upf.edu>
2018-05-22 17:11:08 +0200

    Qulitative/Quantitative

--- API/views.py
+++ API/views.py
@@ -12,7 +12,7 @@ from .serializers import YourSerializer,FindingSerializer
 from API.utils import extract
 
 findings_df = pd.read_pickle("API/static/data/findings.pkl.gz", compression='gzip')
-ontology_df = pd.read_pickle("API/static/data/ontology.pkl")
+onto_df = pd.read_pickle("API/static/data/ontology.pkl")
 study_df = pd.read_pickle("API/static/data/study.pkl")
 
 
@@ -63,11 +63,10 @@ def findings(request):
     sex = request.GET.getlist("sex")
     min_exposure = request.GET.get("min_exposure")
     max_exposure = request.GET.get("max_exposure")
-    relevant = request.GET.get("relevantOnly")
+    relevant = request.GET.get("treatmentRelated")
     page = int(request.GET.get("page"))
 
     global findings_df,study_df
-
 
     #Filter
     ########################################3
@@ -115,8 +114,6 @@ def findings(request):
         init = 0
         end = len(filtered)
 
-    print ("Init "+str(init))
-    print( "End " + str(end))
     # adding the values in a context variable
     num_pages = int(len(filtered) / 20)
 
@@ -148,11 +145,226 @@ def findings(request):
         'page': page,
         'previous_page': previous_page,
         'next_page': next_page,
+        'num_studies': num_studies,
+        'num_structures': num_structures
     }
 
     send_data = FindingSerializer(results, many=False).data
     return Response(send_data)
 
+@api_view(['GET'])
+def qualitative(request):
+
+
+    global findings_df,study_df
+
+    all_organs = request.GET.getlist("organs")
+    all_observation = request.GET.getlist("observations")
+    all_species = request.GET.getlist("species")
+    all_routes = request.GET.getlist("routes")
+    sex = request.GET.getlist("sex")
+    min_exposure = request.GET.get("min_exposure")
+    max_exposure = request.GET.get("max_exposure")
+    relevant = request.GET.get("treatmentRelated")
+
+    global findings_df,study_df
+
+    #############
+    # Filter    #
+    #############
+
+    filter_dict = {}
+    if min_exposure:
+        filter_dict['min_exposure'] = int(min_exposure[0])
+    if max_exposure:
+        filter_dict['max_exposure'] = int(max_exposure[0])
+    if len(all_routes) > 0:
+        filter_dict['route'] = all_routes
+    if len(all_species) > 0:
+        filter_dict['species'] = all_species
+
+
+    filtered = extract.filter_study(filter_dict, study_df)
+    filtered = findings_df[findings_df.study_id.isin(filtered.study_id)]
+
+    filtered = pd.merge(filtered[['study_id', 'observation_normalised', 'organ_normalised', 'dose', \
+                                  'relevance', 'normalised_sex']],
+                        study_df[['study_id', 'subst_id', 'normalised_administration_route', \
+                               'normalised_species', 'exposure_period_days', 'report_number']],
+                        how='left', on='study_id', left_index=False,
+                        right_index=False, sort=False)
+
+    if sex:
+        filtered = filtered[filtered.normalised_sex.str.lower() == sex[0].lower()]
+    if relevant:
+        filtered = filtered[filtered.relevance == 'treatment related']
+    if len(all_organs) > 0:
+        filter_dict['organs'] = all_organs
+        filtered = filtered[filtered['organ_normalised'].isin(all_organs)]
+    if len(all_observation) > 0:
+        filter_dict['observations'] = all_observation
+        filtered = filtered[filtered['observation_normalised'].isin(all_observation)]
+
+
+    ###################################
+    # Get stats for relevant findings #
+    ###################################
+
+    # Get the number of studies per substance
+    count_df = filtered.groupby(('subst_id')).study_id.nunique().to_frame().reset_index()
+    # Get the global dose range per substance
+    range_df = filtered[filtered.dose > 0]
+    range_df = range_df.groupby(('subst_id')).dose.apply(extract.get_stats).unstack().reset_index()
+    # Get all stats into a single dataframe
+    stats_df = pd.merge(count_df, range_df, how='inner', on='subst_id',
+                        left_index=False, right_index=False, sort=False)
+    stats_df.columns = ['subst_id', 'study_count', 'dose_max', 'dose_min']
+
+    # Expand
+    filtered_piv = extract.expand(filtered, filter_dict, onto_df)
+
+    ######################################
+    # Aggragate by substance and finding #
+    ######################################
+    # Define finding as organ+observation
+    filtered_piv.organ_normalised = filtered_piv.organ_normalised.fillna('NA')
+    filtered_piv.observation_normalised = filtered_piv.observation_normalised.fillna('NA')
+    filtered_piv['finding'] = filtered_piv.apply(
+        lambda row: row.organ_normalised + '_' + row.observation_normalised, axis=1)
+    filtered_piv = filtered_piv[['subst_id', 'finding', 'dose']]
+
+    # Aggregate by substance and finding (as defined above), keeping the minimum dose
+    # for each substance/finding instance
+    group_df = filtered_piv.groupby(('subst_id', 'finding')).min().add_prefix('min_').reset_index()
+
+    #######################################
+    # Pivot so that each finding is a row #
+    #######################################
+    ### Qualitative
+    group_df.loc[:, 'min_dose'] = 1
+    pivotted_df = group_df.pivot_table(index='subst_id', columns='finding', values='min_dose').reset_index()
+    pivotted_df['is_active'] = 'True'
+    qualitative_df = pd.merge(stats_df, pivotted_df, how='left', on='subst_id',
+                              left_index=False, right_index=False, sort=False)
+    qualitative_df.is_active = qualitative_df.is_active.fillna('False')
+    # Reorder columns
+    cols = qualitative_df.columns.tolist()
+    cols = cols[0:4] + [cols[-1]] + cols[4:-1]
+    qualitative_df = qualitative_df[cols]
+
+    print (qualitative_df.head())
+
+    results = {
+        "data" : qualitative_df.fillna(value=0)
+    }
+
+
+    return Response(results)
+
+@api_view(['GET'])
+def quantitative(request):
+
+
+    global findings_df,study_df
+
+    all_organs = request.GET.getlist("organs")
+    all_observation = request.GET.getlist("observations")
+    all_species = request.GET.getlist("species")
+    all_routes = request.GET.getlist("routes")
+    sex = request.GET.getlist("sex")
+    min_exposure = request.GET.get("min_exposure")
+    max_exposure = request.GET.get("max_exposure")
+    relevant = request.GET.get("treatmentRelated")
+
+    global findings_df,study_df
+
+    #############
+    # Filter    #
+    #############
+
+    filter_dict = {}
+    if min_exposure:
+        filter_dict['min_exposure'] = int(min_exposure[0])
+    if max_exposure:
+        filter_dict['max_exposure'] = int(max_exposure[0])
+    if len(all_routes) > 0:
+        filter_dict['route'] = all_routes
+    if len(all_species) > 0:
+        filter_dict['species'] = all_species
+
+
+    filtered = extract.filter_study(filter_dict, study_df)
+    filtered = findings_df[findings_df.study_id.isin(filtered.study_id)]
+
+    filtered = pd.merge(filtered[['study_id', 'observation_normalised', 'organ_normalised', 'dose', \
+                                  'relevance', 'normalised_sex']],
+                        study_df[['study_id', 'subst_id', 'normalised_administration_route', \
+                               'normalised_species', 'exposure_period_days', 'report_number']],
+                        how='left', on='study_id', left_index=False,
+                        right_index=False, sort=False)
+
+    if sex:
+        filtered = filtered[filtered.normalised_sex.str.lower() == sex[0].lower()]
+    if relevant:
+        filtered = filtered[filtered.relevance == 'treatment related']
+    if len(all_organs) > 0:
+        filter_dict['organs'] = all_organs
+        filtered = filtered[filtered['organ_normalised'].isin(all_organs)]
+    if len(all_observation) > 0:
+        filter_dict['observations'] = all_observation
+        filtered = filtered[filtered['observation_normalised'].isin(all_observation)]
+
+
+    ###################################
+    # Get stats for relevant findings #
+    ###################################
+
+    # Get the number of studies per substance
+    count_df = filtered.groupby(('subst_id')).study_id.nunique().to_frame().reset_index()
+    # Get the global dose range per substance
+    range_df = filtered[filtered.dose > 0]
+    range_df = range_df.groupby(('subst_id')).dose.apply(extract.get_stats).unstack().reset_index()
+    # Get all stats into a single dataframe
+    stats_df = pd.merge(count_df, range_df, how='inner', on='subst_id',
+                        left_index=False, right_index=False, sort=False)
+    stats_df.columns = ['subst_id', 'study_count', 'dose_max', 'dose_min']
+
+    # Expand
+    filtered_piv = extract.expand(filtered, filter_dict, onto_df)
+
+    ######################################
+    # Aggragate by substance and finding #
+    ######################################
+    # Define finding as organ+observation
+    filtered_piv.organ_normalised = filtered_piv.organ_normalised.fillna('NA')
+    filtered_piv.observation_normalised = filtered_piv.observation_normalised.fillna('NA')
+    filtered_piv['finding'] = filtered_piv.apply(
+        lambda row: row.organ_normalised + '_' + row.observation_normalised, axis=1)
+    filtered_piv = filtered_piv[['subst_id', 'finding', 'dose']]
+
+    # Aggregate by substance and finding (as defined above), keeping the minimum dose
+    # for each substance/finding instance
+    group_df = filtered_piv.groupby(('subst_id', 'finding')).min().add_prefix('min_').reset_index()
+
+    #######################################
+    # Pivot so that each finding is a row #
+    #######################################
+    ### Quantitative
+    pivotted_df = group_df.pivot_table(index='subst_id', columns='finding', values='min_dose').reset_index()
+    pivotted_df['is_active'] = 'True'
+    quantitative_df = pd.merge(stats_df, pivotted_df, how='left', on='subst_id',
+                               left_index=False, right_index=False, sort=False)
+    quantitative_df.is_active = quantitative_df.is_active.fillna('False')
+    # Reorder columns
+    cols = quantitative_df.columns.tolist()
+    cols = cols[0:4] + [cols[-1]] + cols[4:-1]
+    quantitative_df = quantitative_df[cols]
+
+    results = {
+        "data": quantitative_df.fillna(value=0)
+    }
+
+    return Response(results)
 
 @api_view(['GET'])
 def organs(request):
@@ -174,7 +386,6 @@ def organs(request):
         'organs': organs
     }
     return Response(results)
-
 
 @api_view(['GET'])
 def observations(request):
