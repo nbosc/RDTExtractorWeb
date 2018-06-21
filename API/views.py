6233e56f91d0fe665892a9abaa24d02b237b2707
bet-gregori <elisabet.gregori@gmail.com>
2018-06-21 11:37:55 +0200

    Populate the organs and observations tree views depending on the selected category https://github.com/phi-grib/AngularRDTExtractor/issues/13

--- API/views.py
+++ API/views.py
@@ -62,69 +62,64 @@ def source(request):
 def initFindings(request):
     global merged_df
 
-    page = int(request.GET.get("page"))
-
     num_studies = len(merged_df.study_id.unique().tolist())
     num_structures = len(merged_df.subst_id.unique().tolist())
 
-    fullDict = {}
-    fullDict['organs'] = merged_df.organ_normalised.dropna().unique().tolist()
-    #fullDict['organs'].sort()
-    # Create nested dictionary for angular treeviews
-    organs_df = organ_onto_df[organ_onto_df.child_term.isin([x.lower() for x in fullDict['organs']])]
-    organs_df=getValuesForTree(organs_df,organ_onto_df)
-    relations = organs_df.groupby(by='parent_term')['child_term'].apply(list).to_dict()
-    parents = set(relations.keys()) & set(organ_onto_df[organ_onto_df.level == 1].child_term.tolist())
-    fullDict['organs'] = create_dictionary(relations, parents)
+    optionsDict = {}
 
+    optionsDict['sources'] = merged_df.source.dropna().unique().tolist()
+    optionsDict['sources'].sort()
 
-    fullDict['observations'] = merged_df.observation_normalised.dropna().unique().tolist()
-    #fullDict['observations'].sort()
-    # Create nested dictionary for angular treeviews
-    observations_df = observation_onto_df[observation_onto_df.child_term.isin([x.lower() for x in fullDict['observations']])]
-    observations_df = getValuesForTree(observations_df,observation_onto_df)
-    relations = observations_df.groupby(by='parent_term')['child_term'].apply(list).to_dict()
-    parents = set(relations.keys()) & set(observation_onto_df[observation_onto_df.level == 1].child_term.tolist())
-    fullDict['observations'] = create_dictionary(relations, parents)
+    optionsDict['grades'] = merged_df.grade.dropna().unique().tolist()
+    optionsDict['grades'].sort()
 
+    optionsDict['routes'] = merged_df.normalised_administration_route.dropna().unique().tolist()
+    optionsDict['routes'].sort()
 
-    fullDict['grade'] = merged_df.grade.dropna().unique().tolist()
-    fullDict['grade'].sort()
+    optionsDict['sex'] = merged_df.normalised_sex.dropna().unique().tolist()
+    optionsDict['sex'].sort()
 
-    fullDict['routes'] = merged_df.normalised_administration_route.dropna().unique().tolist()
-    fullDict['routes'].sort()
-
-    fullDict['sex'] = merged_df.normalised_sex.dropna().unique().tolist()
-    fullDict['sex'].sort()
-
-    fullDict['species'] = merged_df.normalised_species.dropna().unique().tolist()
-    fullDict['species'].sort()
+    optionsDict['species'] = merged_df.normalised_species.dropna().unique().tolist()
+    optionsDict['species'].sort()
 
     exposure_range = merged_df.exposure_period_days.dropna().unique().tolist()
     exposure_range.sort()
-    fullDict['exposure_min'] = int(exposure_range[0])
-    fullDict['exposure_max'] = int(exposure_range[-1])
+    optionsDict['exposure_min'] = int(exposure_range[0])
+    optionsDict['exposure_max'] = int(exposure_range[-1])
+
+    optionsDict['organs'] = {}
+    optionsDict['observations'] = {}
+    for source in optionsDict['sources']:
+        organs = merged_df[merged_df.source == source].organ_normalised.dropna().unique().tolist()
+        # Create nested dictionary for angular treeviews
+        organs_df = organ_onto_df[organ_onto_df.child_term.isin(organs)]
+        organs_df = getValuesForTree(organs_df,organ_onto_df)
+        relations = organs_df.groupby(by='parent_term')['child_term'].apply(list).to_dict()
+        parents = set(relations.keys()) & set(organ_onto_df[organ_onto_df.level == 1].child_term.tolist())
+        optionsDict['organs'][source] = create_dictionary(relations, parents)
+
+        observations = merged_df.observation_normalised.dropna().unique().tolist()
+        # Create nested dictionary for angular treeviews
+        observations_df = observation_onto_df[observation_onto_df.child_term.isin(observations)]
+        observations_df = getValuesForTree(observations_df,observation_onto_df)
+        relations = observations_df.groupby(by='parent_term')['child_term'].apply(list).to_dict()
+        parents = set(relations.keys()) & set(observation_onto_df[observation_onto_df.level == 1].child_term.tolist())
+        optionsDict['observations'][source] = create_dictionary(relations, parents)
 
     # Pagination
-    if page != 0:
-        init = (int(page) - 1) * 10
-        end = init + 10
+    page = 1
+    init = 0
+    total = len(merged_df)
+    if total < 10:
+        end = total
+        num_pages = total
     else:
-        init = 0
-        end = len(merged_df)
-
-    num_pages = int(len(merged_df) / 10)
+        end = 10
+        num_pages = int(total / 10)    
 
     # Range of pages to show
-    if page < 4:
-        previous = 0
-        nexts = 7
-    elif page > (num_pages - 4):
-        previous = num_pages - 7
-        nexts = num_pages
-    else:
-        previous = page - 4
-        nexts = page + 3
+    previous = 0
+    nexts = 7
     range_pages = range(1, num_pages + 1)[previous:nexts]
     previous_page = page - 1
     next_page = page + 1
@@ -137,7 +132,7 @@ def initFindings(request):
 
     results = {
         'data': output.fillna(value="-").to_dict('records'),
-        'allOptions': fullDict,
+        'allOptions': optionsDict,
         'range_pages': range_pages,
         'num_pages': num_pages,
         'page': page,
@@ -155,11 +150,6 @@ def findings(request):
     global merged_df, compound_df
 
     filtered_tmp = merged_df[:]
-
-    # Sex
-    sex = request.GET.getlist("sex")
-    if sex:
-        filtered_tmp = filtered_tmp[filtered_tmp.normalised_sex.str.lower() == sex[0].lower()]
 
     # Relevancy
     relevant = request.GET.get("treatmentRelated")
@@ -237,7 +227,7 @@ def findings(request):
         for category in tmp_dict:
             tmp_list = '[%s]' %(', '.join(['\'%s\'' %x.strip() for x in tmp_dict[category]]))
             queryList.append('(source == \'%s\' and grade == %s)' %(category.strip(), tmp_list))
-        queryDict['grade'] = ' and '.join(list(queryList))
+        queryDict['grades'] = ' and '.join(list(queryList))
 
     #####################
     # Apply all filters #
@@ -263,7 +253,7 @@ def findings(request):
         else:
             tmp_df = filtered_tmp
         optionsDict['organs'] = tmp_df.organ_normalised.dropna().unique().tolist()
-        #optionsDict['organs'].sort()
+        # optionsDict['organs'].sort()
         #Create nested dictionary for angular treeviews
         organs_df = organ_onto_df[organ_onto_df.child_term.isin([x.lower() for x in optionsDict['organs']])]
         organs_df = getValuesForTree(organs_df, organ_onto_df)
@@ -280,15 +270,13 @@ def findings(request):
         else:
             tmp_df = filtered_tmp
         optionsDict['observations'] = tmp_df.observation_normalised.dropna().unique().tolist()
-        #optionsDict['observations'].sort()
+        # optionsDict['observations'].sort()
         # Create nested dictionary for angular treeviews
         observations_df = observation_onto_df[observation_onto_df.child_term.isin([x.lower() for x in optionsDict['observations']])]
         observations_df = getValuesForTree(observations_df, observation_onto_df)
         relations = observations_df.groupby(by='parent_term')['child_term'].apply(list).to_dict()
         parents = set(relations.keys()) & set(observation_onto_df[observation_onto_df.level == 1].child_term.tolist())
         optionsDict['observations'] = create_dictionary(relations, parents)
-
-
 
         tmp_dict = copy.deepcopy(queryDict)
         tmp_dict.pop('grade', None)
@@ -629,7 +617,7 @@ def study(request):
 #     return Response(send_data)
 
 '''
-Recurive function to create dictionary for treeviews
+Recursive function to create dictionary for treeviews
 '''
 def create_dictionary(relations, parents):
     dict_out = {}
@@ -644,7 +632,7 @@ def create_dictionary(relations, parents):
     return dict_out
 
 '''
-Recurive function to merge two nested dictionaries
+Recursive function to merge two nested dictionaries
 '''
 
 def mergeDeepDict(d1, d2):
