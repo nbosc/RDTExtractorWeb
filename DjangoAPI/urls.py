7a26ddefcc632b2db4f800dfa02d7fdf5afe4fa9
IgnacioPasamontes <ignacio.pasamontes@upf.edu>
2018-10-02 11:55:50 +0200

    Setting and urls changes

--- DjangoAPI/urls.py
+++ DjangoAPI/urls.py
@@ -19,7 +19,6 @@ from django.contrib.staticfiles.views import serve
 
 urlpatterns = [
     url(r'^$', serve, kwargs={'path': 'index.html'}),
-    url(r'^(?!/?static/)(?!/?media/)(?P<path>.*\..*)$',
-    RedirectView.as_view(url='/static/%(path)s', permanent=False)),
+    url(r'^(?!/?static/)(?!/?media/)(?P<path>.*\..*)$',RedirectView.as_view(url='/static/%(path)s', permanent=False)),
     url(r'^', include('API.urls')),  
 ]
