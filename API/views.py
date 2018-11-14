e1bcd48e16acaae1d8188fb4bbcab714341a79a6
bet-gregori <elisabet.gregori@gmail.com>
2018-11-14 12:54:33 +0100

    Fixed bug

--- API/views.py
+++ API/views.py
@@ -456,10 +456,6 @@ def download(request):
 
     global substance_df, filtered, caption
 
-    min_dose = float(request.GET.get("min_dose"))
-
-    print (min_dose)
-
     ####################################
     ## Generate output files          ##
     ## the filtering criteria applied ##
